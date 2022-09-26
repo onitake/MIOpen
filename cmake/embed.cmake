9519120859a8a7bdaac70f705c54f3b7ad68956a
Jun Liu <Liu.Jun@amd.com>
2022-09-25 23:23:21 -0700

    fix issue with embed

--- cmake/embed.cmake
+++ cmake/embed.cmake
@@ -84,6 +84,7 @@ function(generate_embed_source EMBED_NAME)
 
     file(WRITE "${PARSE_HEADER}" "
 #include <unordered_map>
+#include <string>
 const std::unordered_map<std::string, std::pair<const char*,const char*>>& ${EMBED_NAME}();
 ")
 
